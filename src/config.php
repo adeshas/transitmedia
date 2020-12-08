49d02a41f2de0afc94ec27c8325a79b3cdade057
Ade Shas <adeshas@gmail.com>
2020-12-08 21:00:23 +0100

    styling purchase orders

--- src/config.php
+++ src/config.php
@@ -171,7 +171,7 @@ $CONFIG = [
 
     // General
     "UNFORMAT_YEAR" => 50, // Unformat year
-    "RANDOM_KEY" => 'ai1Mb2CqnZ51zCWw', // Random key for encryption
+    "RANDOM_KEY" => 'ZkdeSu2rgz5W1So9', // Random key for encryption
     "ENCRYPTION_KEY" => '', // Encryption key for data protection
     "PROJECT_STYLESHEET_FILENAME" => "css/test.css", // Project stylesheet file name
     "PROJECT_CHARSET" => "utf-8", // Project charset
--- src/config.production.php
+++ src/config.production.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "Zimmerr0hde", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "q1z0Gg9eisS5a8f0", // API Secret Key
+        "SECRET_KEY" => "2qhRw19dfMwiG1Gi", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
