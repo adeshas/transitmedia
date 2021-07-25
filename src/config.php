1dd24c35391f1bd32f3a160fdc3eeded11c3d665
Ade Shas <adeshas@gmail.com>
2021-07-25 22:31:03 +0100

    local copy

--- src/config.php
+++ src/config.php
@@ -171,7 +171,7 @@ $CONFIG = [
 
     // General
     "UNFORMAT_YEAR" => 50, // Unformat year
-    "RANDOM_KEY" => 'hN4EvkHg4WGUHtK4', // Random key for encryption
+    "RANDOM_KEY" => '9lQal4MiNgR0QA9c', // Random key for encryption
     "ENCRYPTION_KEY" => '', // Encryption key for data protection
     "PROJECT_STYLESHEET_FILENAME" => "css/test.css", // Project stylesheet file name
     "PROJECT_CHARSET" => "utf-8", // Project charset
--- views/ZPriceSettingsEdit.php
+++ views/ZPriceSettingsEdit.php
@@ -386,8 +386,8 @@ loadjs.ready("head", function() {
         <div class="<?= $Page->RightColumnClass ?>"><div <?= $Page->active->cellAttributes() ?>>
 <span id="el_z_price_settings_active">
 <div class="custom-control custom-checkbox d-inline-block">
-    <input type="checkbox" class="custom-control-input<?= $Page->active->isInvalidClass() ?>" data-table="z_price_settings" data-field="x_active" name="x_active[]" id="x_active_441648" value="1"<?= ConvertToBool($Page->active->CurrentValue) ? " checked" : "" ?><?= $Page->active->editAttributes() ?> aria-describedby="x_active_help">
-    <label class="custom-control-label" for="x_active_441648"></label>
+    <input type="checkbox" class="custom-control-input<?= $Page->active->isInvalidClass() ?>" data-table="z_price_settings" data-field="x_active" name="x_active[]" id="x_active_684397" value="1"<?= ConvertToBool($Page->active->CurrentValue) ? " checked" : "" ?><?= $Page->active->editAttributes() ?> aria-describedby="x_active_help">
+    <label class="custom-control-label" for="x_active_684397"></label>
 </div>
 <?= $Page->active->getCustomMessage() ?>
 <div class="invalid-feedback"><?= $Page->active->getErrorMessage() ?></div>
